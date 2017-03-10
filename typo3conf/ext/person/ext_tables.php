7dc215da154b63768a30b21ead9b300ac1430eae
Alex Kellner <alexander.kellner@in2code.de>
2017-03-10 15:29:38 +0100

    [TASK] Add FlexForm to plugin

--- typo3conf/ext/person/ext_tables.php
+++ typo3conf/ext/person/ext_tables.php
@@ -11,3 +11,10 @@ if (!defined('TYPO3_MODE')) {
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Person');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_person_domain_model_person');
+
+
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_addlist']['person_pi1'] = 'pi_flexform';
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue(
+    'person_pi1',
+    'FILE:EXT:person/Configuration/FlexForms/FlexForm.xml'
+);
