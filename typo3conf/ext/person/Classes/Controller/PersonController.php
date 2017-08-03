2be8ae32752f742d84875f2216eb74a80b0c98e9
Alex Kellner <alexander.kellner@in2code.de>
2017-08-03 15:07:01 +0200

    [TASK] Add detail view

--- typo3conf/ext/person/Classes/Controller/PersonController.php
+++ typo3conf/ext/person/Classes/Controller/PersonController.php
@@ -1,38 +1,38 @@
 <?php
 namespace Group\Person\Controller;
 
-/***
- *
- * This file is part of the "Personlist" Extension for TYPO3 CMS.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- *  (c) 2017 Alex Kellner <alexander.kellner@in2code.de>, in2code GmbH
- *
- ***/
+use Group\Person\Domain\Model\Person;
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 /**
  * PersonController
  */
-class PersonController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+class PersonController extends ActionController
 {
     /**
-     * personRepository
-     *
      * @var \Group\Person\Domain\Repository\PersonRepository
      * @inject
      */
     protected $personRepository = null;
 
     /**
-     * action list
-     *
      * @return void
      */
     public function listAction()
     {
         $persons = $this->personRepository->findAll();
         $this->view->assign('persons', $persons);
+    }
+
+    /**
+     * &tx_person_pi1[action]=detail
+     * &tx_person_pi1[person]=1
+     *
+     * @param Person $person
+     * @return void
+     */
+    public function detailAction(Person $person)
+    {
+        $this->view->assign('person', $person);
     }
 }
--- typo3conf/ext/person/Classes/Domain/Repository/PersonRepository.php
+++ typo3conf/ext/person/Classes/Domain/Repository/PersonRepository.php
@@ -1,20 +1,11 @@
 <?php
 namespace Group\Person\Domain\Repository;
 
-/***
- *
- * This file is part of the "Personlist" Extension for TYPO3 CMS.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- *  (c) 2017 Alex Kellner <alexander.kellner@in2code.de>, in2code GmbH
- *
- ***/
+use TYPO3\CMS\Extbase\Persistence\Repository;
 
 /**
- * The repository for Persons
+ * Class PersonRepository
  */
-class PersonRepository extends \TYPO3\CMS\Extbase\Persistence\Repository
+class PersonRepository extends Repository
 {
-    }
+}
