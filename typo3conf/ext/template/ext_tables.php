a659290836398fc8ed55982421a3f7ff5c8ba130
Alex Kellner <alexander.kellner@in2code.de>
2017-08-14 16:15:32 +0200

    [TASK] Add default backend layout

--- typo3conf/ext/template/ext_tables.php
+++ typo3conf/ext/template/ext_tables.php
@@ -2,10 +2,16 @@
 
 call_user_func(
     function () {
+        // TypoScript for FE rendering
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
             'template',
             'Configuration/TypoScript',
             'Root Template'
+        );
+
+        // Page TSConfig for backend configuration
+        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+            '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/PageTsConfig/main.typoscript">'
         );
     }
 );
