938e2ab96f173b90ca55c5743a0b20b32cebccd1
Alex Kellner <alexander.kellner@in2code.de>
2017-08-16 15:14:35 +0200

    [TASK] First output in list view

--- typo3conf/ext/employees/Classes/Controller/EmployeeController.php
+++ typo3conf/ext/employees/Classes/Controller/EmployeeController.php
@@ -1,25 +1,14 @@
 <?php
 namespace In2code\Employees\Controller;
 
-/***
- *
- * This file is part of the "EmployeeList" Extension for TYPO3 CMS.
- *
- * For the full copyright and license information, please read the
- * LICENSE.txt file that was distributed with this source code.
- *
- *  (c) 2017 Alex Kellner <alexander.kellner@in2code.de>, in2code.de
- *
- ***/
+use TYPO3\CMS\Extbase\Mvc\Controller\ActionController;
 
 /**
- * EmployeeController
+ * Class EmployeeController
  */
-class EmployeeController extends \TYPO3\CMS\Extbase\Mvc\Controller\ActionController
+class EmployeeController extends ActionController
 {
     /**
-     * employeeRepository
-     *
      * @var \In2code\Employees\Domain\Repository\EmployeeRepository
      * @inject
      */
