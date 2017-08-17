10df2e4aae24840865cbdfd4556ace5642e51e8b
Alex Kellner <alexander.kellner@in2code.de>
2017-08-17 10:01:34 +0200

    [TASK] Advanced filter with split on first and lastname

--- typo3conf/ext/employees/Classes/Domain/Repository/EmployeeRepository.php
+++ typo3conf/ext/employees/Classes/Domain/Repository/EmployeeRepository.php
@@ -2,6 +2,8 @@
 declare(strict_types=1);
 namespace In2code\Employees\Domain\Repository;
 
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Persistence\QueryInterface;
 use TYPO3\CMS\Extbase\Persistence\QueryResultInterface;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
@@ -18,14 +20,29 @@ class EmployeeRepository extends Repository
     public function findByFilter(array $filter): QueryResultInterface
     {
         $query = $this->createQuery();
-        if (!empty($filter['searchterm'])) {
-            $logicalOr = [
-                $query->like('lastName', '%' . $filter['searchterm'] . '%'),
-                $query->like('firstName', '%' . $filter['searchterm'] . '%'),
-                $query->like('description', '%' . $filter['searchterm'] . '%')
-            ];
-            $query->matching($query->logicalOr($logicalOr));
-        }
+        $this->findByFilterWhere($filter, $query);
         return $query->execute();
+    }
+
+    /**
+     * @param array $filter
+     * @param QueryInterface $query
+     * @return void
+     */
+    protected function findByFilterWhere(array $filter, QueryInterface $query)
+    {
+        if (!empty($filter['searchterm'])) {
+            $searchterms = GeneralUtility::trimExplode(' ', $filter['searchterm'], true);
+            $logicalAnd = [];
+            foreach ($searchterms as $searchterm) {
+                $logicalOr = [
+                    $query->like('lastName', '%' . $searchterm . '%'),
+                    $query->like('firstName', '%' . $searchterm . '%'),
+                    $query->like('description', '%' . $searchterm . '%')
+                ];
+                $logicalAnd[] = $query->logicalOr($logicalOr);
+            }
+            $query->matching($query->logicalAnd($logicalAnd));
+        }
     }
 }
