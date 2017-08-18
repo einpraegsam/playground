59093212c17df6c6c3a2e343cc2a298fb092d3a3
Alex Kellner <alexander.kellner@in2code.de>
2017-08-18 13:33:22 +0200

    [TASK] Minimize HTML output

---
+++ typo3conf/ext/template/Classes/Hooks/ContentPostProcessor.php
@@ -0,0 +1,52 @@
+<?php
+namespace In2code\Template\Hooks;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Frontend\Controller\TypoScriptFrontendController;
+use WyriHaximus\HtmlCompress\Factory;
+
+/**
+ * Class ContentPostProcessor needs composer requirement wyrihaximus/html-compress
+ */
+class ContentPostProcessor
+{
+    /**
+     * @param array $parentObject
+     * @param TypoScriptFrontendController $typoScriptFrontend
+     */
+    public function render(array $parentObject, TypoScriptFrontendController $typoScriptFrontend)
+    {
+        if ($this->isDefaultTypeNum()) {
+            if ($this->isCompressBodyActive()) {
+                $parser = Factory::construct();
+
+                $pattern = '~<body.*?>(.*?)<\/body>~is';
+                preg_match($pattern, $typoScriptFrontend->content, $body);
+                $typoScriptFrontend->content = preg_replace(
+                    '/<body.*<\/body>/is',
+                    $parser->compress($body[0]),
+                    $typoScriptFrontend->content
+                );
+            }
+        }
+    }
+
+    /**
+     * @return bool
+     */
+    protected function isDefaultTypeNum()
+    {
+        return GeneralUtility::_GET('type') === null;
+    }
+
+    /**
+     * Enable in TypoScript with config.compressBody=1
+     *
+     * @return bool
+     * @SuppressWarnings(PHPMD.Superglobals)
+     */
+    protected function isCompressBodyActive(): bool
+    {
+        return !empty($GLOBALS['TSFE']->config['config']['compressBody']);
+    }
+}
--- typo3conf/ext/template/ext_localconf.php
+++ typo3conf/ext/template/ext_localconf.php
@@ -10,5 +10,14 @@ call_user_func(
          * CK editor configuration
          */
         $GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['template'] = 'EXT:template/Configuration/Yaml/Rte.yaml';
+
+
+        /**
+         * Compression of HTML output
+         */
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-output'][] =
+            \In2code\Template\Hooks\ContentPostProcessor::class . '->render';
+        $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['tslib/class.tslib_fe.php']['contentPostProc-all'][] =
+            \In2code\Template\Hooks\ContentPostProcessor::class . '->render';
     }
 );
