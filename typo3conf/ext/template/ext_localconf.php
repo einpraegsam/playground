a2a81ca087ef240bf5de28ae8259b255c2eca39a
Alex Kellner <alexander.kellner@in2code.de>
2017-10-25 12:02:43 +0200

    [TASK] Add backend layout "Einspalter"

---
+++ typo3conf/ext/template/ext_localconf.php
@@ -0,0 +1,4 @@
+<?php
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/TSConfig/BackendLayouts/Einspalter.typoscript">'
+);
