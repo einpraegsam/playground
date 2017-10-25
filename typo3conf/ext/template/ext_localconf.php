16fdf033d15bc4c6aad75dfdeee605ea35836924
Alex Kellner <alexander.kellner@in2code.de>
2017-10-25 12:20:35 +0200

    [TASK] TSConfig with Main.typoscript

--- typo3conf/ext/template/ext_localconf.php
+++ typo3conf/ext/template/ext_localconf.php
@@ -1,4 +1,4 @@
 <?php
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
-    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/TSConfig/BackendLayouts/Einspalter.typoscript">'
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/TSConfig/Main.typoscript">'
 );
