dcc119d6d173c7dc05967569d4ecd4ba73912fa1
Alex Kellner <alexander.kellner@in2code.de>
2017-10-25 12:26:50 +0200

    [TASK] Add TypoScript setup in general

--- typo3conf/ext/template/ext_localconf.php
+++ typo3conf/ext/template/ext_localconf.php
@@ -1,4 +1,14 @@
 <?php
+/**
+ * Add Page TSConfig (e.g. for backend layouts)
+ */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
     '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/TSConfig/Main.typoscript">'
+);
+
+/**
+ * Add TypoScript setup for frontend rendering
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTypoScriptSetup(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/TypoScript/Main.typoscript">'
 );
