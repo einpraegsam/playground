6a1a187894c6faa781bf47b448d873656fb418d7
Alex Kellner <alexander.kellner@in2code.de>
2018-04-17 09:45:14 +0200

    [TASK] Add a ext_emconf.php file to allow extension activating in extension manager
    Now, we're able to use EXT: instead of typo3conf/ext/ as path-prefix

---
+++ typo3conf/ext/template/ext_emconf.php
@@ -0,0 +1,16 @@
+<?php
+
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'LRZ Template',
+    'description' => 'Main template extension for LRZ',
+    'author' => 'Alex',
+    'state' => 'stable',
+    'version' => '1.0.0',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '8.7.0-8.7.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [],
+    ],
+];
