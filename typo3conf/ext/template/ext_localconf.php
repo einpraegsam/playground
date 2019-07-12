a05bfde3c66da0bcacaf9b5f70752611548b4c9e
Alex Kellner <alexander.kellner@in2code.de>
2019-07-12 11:31:54 +0200

    [TASK] Disable some fields in backend - also for administrators

---
+++ typo3conf/ext/template/ext_localconf.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Add Page TSConfig
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    '<INCLUDE_TYPOSCRIPT: source="FILE:EXT:template/Configuration/PageTsConfig/Permissions.typoscript">'
+);
