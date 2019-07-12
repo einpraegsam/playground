44dd32da8a990e875f9e1d0d4e06eade147224cd
Alex Kellner <alexander.kellner@in2code.de>
2019-07-12 08:51:33 +0200

    [TASK] Change the way TypoScript is included
    Now it's possible to select our TypoScript fom static template selector

---
+++ typo3conf/ext/template/Configuration/TCA/Overrides/sys_template.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Include TypoScript
+ */
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
+    'template',
+    'Configuration/TypoScript',
+    'Root template for website'
+);
