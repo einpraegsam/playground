f178ff077a99038595a0fb2bb5e63ef05598f261
Alex Kellner <alexander.kellner@in2code.de>
2019-11-04 13:02:38 +0100

    [TASK] Extension Manager configuration added

---
+++ public/typo3conf/ext/personregister/ext_emconf.php
@@ -0,0 +1,15 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'person register',
+    'description' => 'Person register',
+    'version' => '1.0.0',
+    'state' => 'stable',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '9.5.0-9.5.99'
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [],
+    ]
+];
